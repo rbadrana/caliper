37d7fe1f00c054becbf498d0ce661c4ca7ed932d
rajesh <rbadrana@gmail.com>
2021-12-31 16:20:13 +0000

    getOrgTmp trial

---
+++ workload/getOrgTmp.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class MyWorkload extends WorkloadModuleBase {
+    constructor() {
+        super();
+    }
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+    }
+    
+    async submitTransaction() {
+        //const randomId = Math.floor(Math.random()*this.roundArguments.assets);
+        const myArgs = {
+            contractId: 'orgtmp',
+            contractVersion: 'v3',
+            contractFunction: 'getOrgTmp',
+            invokerIdentity: 'client0.org1.example.com',
+            contractArguments: ['Org1Sensor1'],
+            readOnly: false
+        };
+        await this.sutAdapter.sendRequests(myArgs);
+    }
+    
+    async cleanupWorkloadModule() {
+        // NOOP
+    }
+}
+function createWorkloadModule() {
+    return new MyWorkload();
+}
+module.exports.createWorkloadModule = createWorkloadModule;
